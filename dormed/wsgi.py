fe4519b8756d32bce1dc2589ca0eee7555185d62
DemetrPI <demetriy.78@gmail.com>
2024-08-26 15:28:52 +0200

    remove Azure links in wsgi.py

--- dormed/wsgi.py
+++ dormed/wsgi.py
@@ -12,9 +12,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dormed.settings')
 application = get_wsgi_application()
 
-# If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
-# on Azure App Service and should use the production settings in production.py.
-settings_module = "dormed.production" if 'WEBSITE_HOSTNAME' in os.environ else 'dormed.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+# # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
+# # on Azure App Service and should use the production settings in production.py.
+# settings_module = "dormed.production" if 'WEBSITE_HOSTNAME' in os.environ else 'dormed.settings'
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
