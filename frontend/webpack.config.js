1a5c830e3b9f93f2d1a874d993c0bc9b2ac3222b
DemetrPI <demetriy.78@gmail.com>
2025-08-09 11:18:43 +0200

    check for path

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,14 +6,8 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   entry: "./src/index.js",
   output: {
-    // Corrected: This is the root directory where all your compiled frontend assets will go.
-    // It will be 'frontend/static/frontend/' relative to your 'frontend' directory.
     path: path.resolve(__dirname, "static/frontend/"),
-    // This defines the filename for your main JavaScript bundle.
-    // It will be placed directly in the output.path (e.g., frontend/static/frontend/main.js)
     filename: "[name].js",
-    // This publicPath helps Webpack resolve asset URLs correctly in the final built files.
-    // If Django serves your frontend static files from '/static/frontend/', this is the correct public path.
     publicPath: '/static/frontend/',
   },
   module: {
@@ -36,8 +30,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          // Images will be placed in an 'images/' subfolder relative to the output.path.
-          // So, they'll be at frontend/static/frontend/images/[name][ext]
           filename: 'images/[name][ext]'
         }
       }
@@ -49,7 +41,6 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
-        // This has effect on the react lib size
         NODE_ENV: JSON.stringify("development"),
       },
     }),
